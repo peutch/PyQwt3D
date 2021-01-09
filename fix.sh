fb6b49ab6fd0b4df25c978fea123e8ba952a0c0d
Gudjon I. Gudjonsson <gudjon@fessender>
2021-01-10 00:41:22 +0100

    Add a script that creates empty files for the build. Will be removed later.

---
+++ fix.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#Create empty files that are needed for the build. Will be moved 
+#to the build scripts or the references removed.
+
+touch numpy/sip_Qwt3DFreeVector.h
+touch numpy/sip_Qwt3DTriple.h
+touch numpy/sip_Qwt3DDoubleVector.h
+touch numpy/sip_Qwt3DColorVector.h
+touch numpy/sip_Qwt3DAxisVector.h
+touch numpy/sip_Qwt3DCellField.h
+touch numpy/sip_Qwt3DTripleField.h
